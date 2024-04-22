51e6912be6079f3e6741a4d18293c20a1f524409
Raí Joia <megac154@gmail.com>
2024-04-21 23:35:55 -0300

    feat: initial project

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`servidor está rodando na porta ${port}`));
+
+module.exports = app;
